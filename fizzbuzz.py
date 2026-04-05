0e6a0a67fa21585d257df2ac23ed157320e02e9e
Jason R. Surratt <jason.surratt@gmail.com>
2026-04-05 11:03:58 +0000

    feat: add fizzbuzz script

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+"""FizzBuzz implementation."""
+
+
+def fizzbuzz(n: int) -> str:
+    if n % 15 == 0:
+        return "FizzBuzz"
+    if n % 3 == 0:
+        return "Fizz"
+    if n % 5 == 0:
+        return "Buzz"
+    return str(n)
+
+
+if __name__ == "__main__":
+    for i in range(1, 101):
+        print(fizzbuzz(i))
